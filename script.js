94b5b48771209c26cdc479b60976afe9f52de2d1
rajeevrk9901 <rajeevrk9901@gmail.com>
2021-11-02 08:10:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuUppercase = document.getElementById("menu1st").textContent
+
+// const uppercase = function (menuUppercase){
+//     return menuUppercase.toUpperCase();
+// }
+
+// uppercase();
+
+menuUppercase.toUpperCase();
+
+console.log(typeof menuUppercase);
+
+console.log(menuUppercase.toUpperCase());
